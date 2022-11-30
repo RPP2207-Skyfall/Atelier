9427333027d17d40df0186a5d658b64ecc081f76
Josh Krebs <joddykrebs4@gmail.com>
2022-11-29 19:30:25 -0800

    delete key information

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -5,7 +5,6 @@ import AddToCart from './parts/addToCart/AddToCart.jsx';
 import ImageGallery from './parts/ImageGallery/ImageGallery.jsx';
 import axios from 'axios';
 
-const API_KEY = 'https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rpp/products';
 
 
 function Overview() {
@@ -13,25 +12,6 @@ function Overview() {
 
   const [info, getInfo] = useState([]);
 
-  // useEffect(() => {
-
-  //   fetch (API_KEY,
-  //       {
-  //         method: "GET",
-  //         headers:
-  //           {
-  //             Authorization: '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'
-  //           }
-  //       }
-  //     )
-  //     .then(res => res.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     })
-  //     .catch((err) => {
-  //       console.error(err);
-  //     })
-  // })
 
   return (
     <div>
