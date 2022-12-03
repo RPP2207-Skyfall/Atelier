dedcf988790a8e7222550a064645857a22567ac8
Josh Krebs <joddykrebs4@gmail.com>
2022-12-03 11:54:00 -0800

    pushing so I can pull

--- client/src/components/overview/Overview.test.js
+++ client/src/components/overview/Overview.test.js
@@ -1,6 +1,6 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import Overview from './Overview';
+/**
+ * @jest-environment jsdom
+ */
 
 require('react');
 
