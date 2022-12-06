13f2e947af7825210e94b3fdfc248030c06cce9c
Lenord Chen <chenlenord@gmail.com>
2022-12-05 19:32:03 -0800

    example

--- client/src/components/relatedItem/parts/RelatedItem/RelatedCard.jsx
+++ client/src/components/relatedItem/parts/RelatedItem/RelatedCard.jsx
@@ -21,3 +21,32 @@ const RelatedCard = (props) => (
 
 
 export default RelatedCard;
+
+//data type
+// [
+//   {
+//         "id": 1,
+//         "name": "Camo Onesie",
+//         "slogan": "Blend in to your crowd",
+//         "description": "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
+//         "category": "Jackets",
+//         "default_price": "140"
+//     },
+//   {
+//         "id": 2,
+//         "name": "Bright Future Sunglasses",
+//         "slogan": "You've got to wear shades",
+//         "description": "Where you're going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.",
+//         "category": "Accessories",
+//         "default_price": "69"
+//     },
+//   {
+//         "id": 3,
+//         "name": "Morning Joggers",
+//         "slogan": "Make yourself a morning person",
+//         "description": "Whether you're a morning person or not. Whether you're gym bound or not. Everyone looks good in joggers.",
+//         "category": "Pants",
+//         "default_price": "40"
+//     },
+//     // ...
+// ]
