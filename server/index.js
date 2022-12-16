ff61efd21f8f0809e62f082cdb137725721407a3
Lenord Chen <chenlenord@gmail.com>
2022-12-15 20:11:35 -0800

    API router setup on server

--- server/index.js
+++ server/index.js
@@ -4,15 +4,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000
 const Axios = require('axios')
-
 const routes = require('./routes')
+const controller = require('./controllers/')
+
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
 // Routes
 app.use(routes)
-
-
 
 
 
--- server/routes.js
+++ server/routes.js
@@ -1,9 +1,15 @@
 require("dotenv").config();
 const router = require("express").Router();
 const controller = require('./controllers/')
+// router.route('/getItemDetails')
+//   .get(controller.itemDetails.getItemDetails)
 
-router.get('/review', controller.review.get_product_reviews)
-router.get('/metadata', controller.review.get_metadata)
+// router.get('/review', controller.review.get_product_reviews)
+// router.get('/metadata', controller.review.get_metadata)
+router.get('/relateItemsID', controller.relatedID.getRelatedItemsID)
+router.get('/getRating', controller.rating.getRating)
+router.get('/getFirstImage', controller.image.getFirstImage)
+router.get('/getItemDetails', controller.itemDetails.getItemDetails)
 
 
 
