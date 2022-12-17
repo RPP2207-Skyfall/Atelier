6a5e4231d152ed4604940a60b01c5f36ef85070a
Howard Yoon <howardhyoon@gmail.com>
2022-12-18 05:23:00 +0900

    Found the root cause of not updating props. Adressed and now Search bar works great

--- client/src/components/QA/Search.jsx
+++ client/src/components/QA/Search.jsx
@@ -6,26 +6,15 @@ import Stack from '@mui/material/Stack';
 
 //after typing of 3 characters, start querying
 
-class Search extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      query:''
-    }
-  };
-
-  render() {
-    return(
+const Search = (props) => (
       <div className='question-and-answer-search-bar' data-testid='question-and-answer-search-bar'>
         <Stack direction='row' spacing={1}>
-          <TextField fullWidth id='search-bar' placeholder='HAVE A QUESTION? SEARCH FOR ANSWER...' onChange={(e) => {this.props.handleSearch(e.target.value)}}></TextField>
+          <TextField fullWidth id='search-bar' placeholder='HAVE A QUESTION? SEARCH FOR ANSWERS...' onChange={(e) => {props.handleSearch(e.target.value)}}></TextField>
             <IconButton color='primary' aria-label='search QA'>
               <SearchIcon id='search-icon'/>
             </IconButton>
         </Stack>
       </div>
-    )
-  }
-};
+);
 
 export default Search;
