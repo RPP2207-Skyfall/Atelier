6ab5d438a0f03cbeb36eba8290acb43f722b2bc1
Jason Kwong <skjKwong@gmail.com>
2023-01-04 15:24:04 -0800

    added preload into link tags

--- client/src/components/overview/parts/styleSelector/StyleSelector.jsx
+++ client/src/components/overview/parts/styleSelector/StyleSelector.jsx
@@ -16,25 +16,25 @@ function StyleSelector(props) {
         <div className="styles-holder">
           {
 
-                props.styles.results.map((style, i) => {
+            props.styles.results.map((style, i) => {
 
-                  // console.log('style in selector: ', style)
-                  if (style.style_id === props.currentStyle.style_id) {
-                    return (
-                      <div key={i} className="current-style-selected" onClick={() => props.updateStyle(style, {elem: "current-style-selected", time: Date.now() })}>
-                        {/* <FontAwesomeIcon icon="fa-thin fa-circle-check" /> */}
-                        <img className="selected-style-icon" src="checkmark.png"></img>
-                        <img className="style-image-selected" src={style.photos[0].thumbnail_url} key={i} />
-                      </div>
-                    )
-                  } else {
-                    return (
-                      <div key={i} className="current-style" onClick={() => props.updateStyle(style, {elem: "current-style", time: Date.now() })}>
-                        <img className="style-image" src={style.photos[0].thumbnail_url} key={i} />
-                      </div>
-                    )
-                  }
-                })
+              // console.log('style in selector: ', style)
+              if (style.style_id === props.currentStyle.style_id) {
+                return (
+                  <div key={i} className="current-style-selected" onClick={() => props.updateStyle(style, { elem: "current-style-selected", time: Date.now() })}>
+
+                    <img className="selected-style-icon" src="checkmark.png"></img>
+                    <img className="style-image-selected" src={style.photos[0].thumbnail_url} key={i} />
+                  </div>
+                )
+              } else {
+                return (
+                  <div key={i} className="current-style" onClick={() => props.updateStyle(style, { elem: "current-style", time: Date.now() })}>
+                    <img className="style-image" src={style.photos[0].thumbnail_url} key={i} />
+                  </div>
+                )
+              }
+            })
           }
         </div>
 
