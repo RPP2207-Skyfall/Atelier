e81aaab4eaf22bd5bbba8fbf4c57b499a3a5e158
Lenord Chen <chenlenord@gmail.com>
2023-01-06 15:08:54 -0800

    lazy load for photo box

--- client/src/components/relatedItem/parts/PopUp/HoverPhoto.jsx
+++ client/src/components/relatedItem/parts/PopUp/HoverPhoto.jsx
@@ -1,9 +1,18 @@
 import React, { useState, useEffect } from 'react';
 
 const SmallPicBox = (props) => {
-  const url =  props.item.thumbnail_url
+  const startLoad = props.libraryLoad
+  const [url, setURL] = useState("Loading_icon.gif")
+
+  useEffect(() => {
+    if (startLoad === true) {
+      setURL(props.item.thumbnail_url)
+    }
+  }, [startLoad])
+  // const loading = "Loading_icon.gif"
+  // const url =  props.item.thumbnail_url
   return (
-    <div className="small-box" aria-label = {`other item photos`} style={{ backgroundImage: "url('" + url + "')" }} onClick= {()=>{props.setCurPhoto(url)}}></div>
+     <div className="small-box" aria-label = {`other item photos`} style={{ backgroundImage: `url(${url})` }} onClick= {()=>{props.setCurPhoto(props.item.thumbnail_url)}}></div>
   )
 }
 
